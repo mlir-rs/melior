5c6f7b1056d173e26ebe4f18ed68237b95c396a5
Edgar <git@edgl.dev>
2025-04-02 05:24:59 +0200

    Update to llvm 20 (#687)
    Updated to llvm 20
    
    Added more conversions too
    
    Added distinct builtin attr

--- melior/src/pass/transform.rs
+++ melior/src/pass/transform.rs
@@ -3,15 +3,22 @@
 melior_macro::passes!(
     "Transforms",
     [
+        // spell-checker: disable-next-line
         mlirCreateTransformsCSE,
         mlirCreateTransformsCanonicalizer,
+        mlirCreateTransformsCompositeFixedPointPass,
         mlirCreateTransformsControlFlowSink,
         mlirCreateTransformsGenerateRuntimeVerification,
         mlirCreateTransformsInliner,
         mlirCreateTransformsLocationSnapshot,
         mlirCreateTransformsLoopInvariantCodeMotion,
+        mlirCreateTransformsLoopInvariantSubsetHoisting,
+        mlirCreateTransformsMem2Reg,
+        mlirCreateTransformsPrintIRPass,
         mlirCreateTransformsPrintOpStats,
+        mlirCreateTransformsRemoveDeadValues,
         mlirCreateTransformsSCCP,
+        mlirCreateTransformsSROA,
         mlirCreateTransformsStripDebugInfo,
         mlirCreateTransformsSymbolDCE,
         mlirCreateTransformsSymbolPrivatize,
