1f2ed3c019acbf30069fddd74192b52ce89aceab
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2025-04-07 13:09:00 -0400

    Move ICmp to attributes.rs

--- melior/src/dialect/llvm.rs
+++ melior/src/dialect/llvm.rs
@@ -13,7 +13,7 @@ use crate::{
     Context,
 };
 pub use alloca_options::*;
-use attributes::Linkage;
+use attributes::{ICmpPredicate, Linkage};
 use global::GlobalValue;
 pub use load_store_options::*;
 
@@ -369,69 +369,6 @@ pub fn zext<'c>(
         .expect("valid operation")
 }
 
-// https://github.com/llvm/llvm-project/blob/600eeed51f538adc5f43c8223a57608e73aba31f/mlir/include/mlir/Dialect/LLVMIR/LLVMEnums.td#L542-L558
-/// LLVM float comparison predicates.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-#[repr(u64)]
-pub enum FCmpPredicate {
-    /// Always returns false.
-    False = 0,
-    /// Ordered and equal.
-    Oeq = 1,
-    /// Ordered and greater than.
-    Ogt = 2,
-    /// Ordered and greater than or equal.
-    Oge = 3,
-    /// Ordered and less than.
-    Olt = 4,
-    /// Ordered and less than or equal.
-    Ole = 5,
-    /// Ordered and not equal.
-    One = 6,
-    /// Ordered (no NaNs).
-    Ord = 7,
-    /// Unordered or equal.
-    Ueq = 8,
-    /// Unordered or greater than.
-    Ugt = 9,
-    /// Unordered or greater than or equal.
-    Uge = 10,
-    /// Unordered or less than.
-    Ult = 11,
-    /// Unordered or less than or equal.
-    Ule = 12,
-    /// Unordered or not equal.
-    Une = 13,
-    /// Unordered (either NaNs).
-    Uno = 14,
-    /// Always returns true.
-    True = 15,
-}
-/// LLVM integer comparison predicates.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-#[repr(u64)]
-pub enum ICmpPredicate {
-    /// Equal
-    Eq = 0,
-    /// Not equal
-    Ne = 1,
-    /// Signed less than
-    Slt = 2,
-    /// Signed less than or equal
-    Sle = 3,
-    /// Signed greater than
-    Sgt = 4,
-    /// Signed greater than or equal
-    Sge = 5,
-    /// Unsigned less than
-    Ult = 6,
-    /// Unsigned less than or equal
-    Ule = 7,
-    /// Unsigned greater than
-    Ugt = 8,
-    /// Unsigned greater than or equal
-    Uge = 9,
-}
 /// Creates an `llvm.icmp` operation in Melior.
 /// This is used to create integer AND pointer comparisons, unlike arith::icmp.
 /// The predicate is an enum that specifies the type of comparison to be performed.
--- melior/src/dialect/llvm/attributes.rs
+++ melior/src/dialect/llvm/attributes.rs
@@ -26,3 +26,67 @@ pub fn linkage(context: &Context, linkage: Linkage) -> Attribute {
     };
     Attribute::parse(context, &format!("#llvm.linkage<{linkage}>")).unwrap()
 }
+
+// https://github.com/llvm/llvm-project/blob/600eeed51f538adc5f43c8223a57608e73aba31f/mlir/include/mlir/Dialect/LLVMIR/LLVMEnums.td#L542-L558
+/// LLVM float comparison predicates.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(u64)]
+pub enum FCmpPredicate {
+    /// Always returns false.
+    False = 0,
+    /// Ordered and equal.
+    Oeq = 1,
+    /// Ordered and greater than.
+    Ogt = 2,
+    /// Ordered and greater than or equal.
+    Oge = 3,
+    /// Ordered and less than.
+    Olt = 4,
+    /// Ordered and less than or equal.
+    Ole = 5,
+    /// Ordered and not equal.
+    One = 6,
+    /// Ordered (no NaNs).
+    Ord = 7,
+    /// Unordered or equal.
+    Ueq = 8,
+    /// Unordered or greater than.
+    Ugt = 9,
+    /// Unordered or greater than or equal.
+    Uge = 10,
+    /// Unordered or less than.
+    Ult = 11,
+    /// Unordered or less than or equal.
+    Ule = 12,
+    /// Unordered or not equal.
+    Une = 13,
+    /// Unordered (either NaNs).
+    Uno = 14,
+    /// Always returns true.
+    True = 15,
+}
+/// LLVM integer comparison predicates.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(u64)]
+pub enum ICmpPredicate {
+    /// Equal
+    Eq = 0,
+    /// Not equal
+    Ne = 1,
+    /// Signed less than
+    Slt = 2,
+    /// Signed less than or equal
+    Sle = 3,
+    /// Signed greater than
+    Sgt = 4,
+    /// Signed greater than or equal
+    Sge = 5,
+    /// Unsigned less than
+    Ult = 6,
+    /// Unsigned less than or equal
+    Ule = 7,
+    /// Unsigned greater than
+    Ugt = 8,
+    /// Unsigned greater than or equal
+    Uge = 9,
+}
