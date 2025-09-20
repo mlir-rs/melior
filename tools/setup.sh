5ef3a8b995bc9b7327558dfeff6224e8f6f83b12
Yota Toyama <raviqqe@gmail.com>
2025-09-19 19:36:11 -0700

    Fix

--- tools/setup.sh
+++ tools/setup.sh
@@ -10,12 +10,6 @@ if [ $(uname) = Linux ]; then
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -s
 fi
 
-# TODO Remove this if Homebrew is fixed.
-if [ $(uname) = Darwin ]; then
-  brew uninstall --ignore-dependencies zstd
-  brew install -s zstd
-fi
-
 brew install llvm@$llvm_version
 
 echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
