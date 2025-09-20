349616bc05da3a6e934dd7b103d70d7481d1abd1
Yota Toyama <raviqqe@gmail.com>
2025-09-19 19:29:41 -0700

    fxi

--- tools/setup.sh
+++ tools/setup.sh
@@ -10,7 +10,8 @@ if [ $(uname) = Linux ]; then
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -s
 fi
 
-brew uninstall zstd
+brew uninstall --ignore-dependencies zstd
+brew install -s zstd
 brew install llvm@$llvm_version
 
 echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
