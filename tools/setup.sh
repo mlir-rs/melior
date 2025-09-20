e1d1af6d976f59ec7f96c56d48f187b8a87a21b7
Yota Toyama <raviqqe@gmail.com>
2025-09-19 19:27:44 -0700

    Fix

--- tools/setup.sh
+++ tools/setup.sh
@@ -10,7 +10,7 @@ if [ $(uname) = Linux ]; then
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -s
 fi
 
-brew reinstall zstd
+brew uninstall zstd
 brew install llvm@$llvm_version
 
 echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
