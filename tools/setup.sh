ce4bf19424390e9cd6f5f23a1717a10957d8ef3f
Yota Toyama <raviqqe@gmail.com>
2025-09-19 20:07:56 -0700

    Fix clippy (#723)
    This PR fixes Clippy errors from its recent rerealse.
    
    Also, we now enable only the Ubuntu ARM64 platforms on CI as it is the
    only one which can run the tests reliably as of September 19, 2025.

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,7 +2,13 @@
 
 set -e
 
+[ -n "$CI" ]
+
 llvm_version=20
+
+if [ $(uname) = Linux ]; then
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -s
+fi
 
 brew install llvm@$llvm_version
 
