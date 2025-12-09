e6688953fd2b57ec662bccd2132be24462fe25ab
Yota Toyama <raviqqe@gmail.com>
2025-12-09 08:31:44 -0800

    Fix

--- macro/src/dialect/utility.rs
+++ macro/src/dialect/utility.rs
@@ -114,7 +114,7 @@ mod tests {
     fn sanitize_code_blocks() {
         assert_eq!(
             &sanitize_documentation("```\nfoo\n```\n\n```\nbar\n```\n").unwrap(),
-            "```text\nfoo\n```\n\n``` text\nbar\n```\n"
+            "```text\nfoo\n```\n\n```text\nbar\n```\n"
         );
     }
 
