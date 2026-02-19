b100639e763676d6a86b4415d58d8191d7a696ca
Yota Toyama <raviqqe@gmail.com>
2026-02-18 19:47:36 -0800

    Fix

--- melior/tests/expand_dialect.rs
+++ melior/tests/expand_dialect.rs
@@ -7,3 +7,9 @@ dialect! {
     files: ["IR/AffineOps.td", "TransformOps/AffineTransformOps.td", "IR/AffineMemoryOpInterfaces.td"],
     include_directories: ["mlir/Dialect/Affine"],
 }
+
+dialect! {
+    name: "arith",
+    files: ["mlir/Dialect/Arith/IR/ArithOps.td"],
+    include_directories: [env!("FOO")],
+}
